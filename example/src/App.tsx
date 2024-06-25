094b6b6063b54fc84a69e6397343df383ea3162f
tauceti12 <56082470+tauceti12@users.noreply.github.com>
2024-06-25 07:51:27 +0200

    android working example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,30 +1,69 @@
-import * as React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
 import { decodeQR } from 'react-native-qr-decoder';
+import { launchImageLibrary } from 'react-native-image-picker';
+
+type QR = {
+  name: string;
+  url: string;
+};
 
 export default function App() {
-  const [result, setResult] = React.useState();
+  const [result, setResult] = useState<QR[]>([]);
+  const [imageUri, setImageUri] = useState('');
+  const [openImgLib, setOpenImgLib] = useState(false);
 
-  const exampleImage = require('./qr-code.png');
-
-  const barcode = async () =>
-    decodeQR(exampleImage)
-      .then((data) => {
-        setResult(data);
-        console.log('QR', data);
-      })
-      .catch((error) => {
-        console.error(error);
+  const openImageLibrary = async () => {
+    try {
+      const response = await launchImageLibrary({
+        mediaType: 'photo',
+        selectionLimit: 1,
       });
 
-  React.useEffect(() => {
-    console.log(exampleImage);
-    barcode();
-  }, [exampleImage]);
+      if (response.assets && response.assets.length > 0) {
+        const uri = response.assets[0].uri;
+        setImageUri(uri);
+      }
+    } catch (error) {
+      console.error('Error opening image library: ', error);
+    }
+  };
+
+  useEffect(() => {
+    if (openImgLib) {
+      openImageLibrary();
+      setOpenImgLib(false);
+    }
+  }, [openImgLib]);
+
+  useEffect(() => {
+    if (imageUri) {
+      decodeQR(imageUri)
+        .then((data) => {
+          setResult(data);
+          console.log('QR code result:', data);
+        })
+        .catch((error) => {
+          console.error('Error decoding QR code: ', error);
+        });
+    }
+  }, [imageUri]);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Button
+        title="Open Image Library"
+        color="#841584"
+        onPress={() => setOpenImgLib(true)}
+      />
+      {imageUri ? (
+        <Image source={{ uri: imageUri }} style={styles.image} />
+      ) : null}
+      {result ? (
+        result.map(() => <Text>{result[0].url}</Text>)
+      ) : (
+        <Text>No QR code decoded.</Text>
+      )}
     </View>
   );
 }
@@ -34,10 +73,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
   },
-  box: {
-    width: 60,
-    height: 60,
+  image: {
+    width: 200,
+    height: 200,
     marginVertical: 20,
   },
 });
